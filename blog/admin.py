abd0978e3184c04150c83d9c84ea21956bd1cc33
jesus <jcd010795@gmail.com>
2026-01-05 22:42:10 -0600

    blog: Implementation of the initial Post and Category models
    -The Post model has been created, including fields for title, content, and metadata. -A many-to-many relationship has been established between Posts and Categories. - Automatic timestamp fields (created, updated) have been added for both models.

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from .models import Category, Post
 
-# Register your models here.
+
+class PostModelAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+
+class CategoryModelAdmin(admin.ModelAdmin):
+    readonly_fields = ("created", "updated")
+
+
+admin.site.register(Post, PostModelAdmin)
+admin.site.register(Category, CategoryModelAdmin)
--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,44 @@
+from django.contrib.auth.models import User
+from django.urls import reverse
+from django.utils import timezone
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, verbose_name="Nombre")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de modificacion")
+
+    class Meta:
+        verbose_name = "Categoria"
+        verbose_name_plural = "Categorias"
+        ordering = ["-name"]
+
+    def __str__(self):
+        return self.name
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=60, verbose_name="Titulo")
+    content = models.TextField(verbose_name="Contenido")
+    user = models.ForeignKey(User, verbose_name="Autor", on_delete=models.CASCADE)
+    categories = models.ManyToManyField(
+        Category,
+        verbose_name="Categorias",
+        related_name="posts",
+    )
+    image = models.ImageField(blank=True, null=True, verbose_name="Imagen destacada")
+    created = models.DateTimeField(auto_now_add=True, verbose_name="Fecha de creacion")
+    updated = models.DateTimeField(auto_now=True, verbose_name="Fecha de modificacion")
+    slug = models.SlugField(null=True)
+
+    class Meta:
+        verbose_name = "Publicacion"
+        verbose_name_plural = "Publicaciones"
+        ordering = ["-created"]
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse("post_detail", kwargs={"slug": self.slug})
